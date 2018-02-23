0a802d8ea7c9f5524315b08f56063537b7390e19
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-23 10:51:12 +0530

    Commit1

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../loany.zip');
+var kuduApi = 'https://loany.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$loany';
+var password = 'qA2LQrSNwDfGmZytQlnjMnSk2heARyce9ZGAhCbx05J3uXooSnntBzWqzGpL';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('loany publish');
+  } else {
+    console.error('failed to publish loany', err);
+  }
+});
